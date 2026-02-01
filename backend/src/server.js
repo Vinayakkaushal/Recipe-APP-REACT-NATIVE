404c96d19325ad153533162e525758193759c605
Vinayak Kaushal <vinayakkaushal935@gmail.com>
2026-02-01 22:59:32 +0530

    new changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import { ENV } from "./config/env.js";
 import {db} from "./config/db.js"
 import { favoritesTable } from "./db/schema.js";
+import { and, eq } from "drizzle-orm";
 
 const app = express();
 const port = ENV.PORT
@@ -39,6 +40,33 @@ app.post("/api/favorites", async (req, res) => {
   }
 });
 
+
+app.get("/api/favorites/:userId" , async (req, res) =>{
+  try {
+    const {userId} = req.params;
+    
+    const data = await db.select().from(favoritesTable).where(eq(favoritesTable.userId,userId))
+    console.log(data)
+    res.status(200).json({message: "Data fetched successfully..." , data:data});
+  } catch (error) {
+        console.log("Error fetching favorites", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+})
+
+app.delete("/api/favorites/:userId/:recipeId" , async (req , res)=>{
+  try {
+    const {userId , recipeId} = req.params;
+await db.delete(favoritesTable).where(and(eq(favoritesTable.userId,userId) , eq(favoritesTable.recipeId,parseInt(recipeId))));
+    console.log(data)
+    res.status(200).json({message: "Favorite deleted sucessfully", });
+
+  } catch (error) {
+    
+    console.log("Error deleting favorites", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+})
 
 app.listen(port , ()=>{
     console.log("Server is running")
